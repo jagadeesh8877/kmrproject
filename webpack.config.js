ae0b45ac16fddea16fea98fafe76096a4b376396
jagadeesh8877 <jagadeesh.kagitha@gmail.com>
2018-08-19 17:00:59 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+var config = {
+    entry: './main.js',
+    output: {
+       path:'/',
+       filename: 'index.js',
+    },
+    devServer: {
+       inline: true,
+       port: 8080
+    },
+    module: {
+       rules: [
+          {
+             test: /\.jsx?$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader',
+             query: {
+                presets: ['es2015', 'react', 'env']
+             },
+          },
+          {
+            test:/\.css$/,
+            use:['style-loader','css-loader']
+        }
+       ]
+    }
+ }
+ module.exports = config;
